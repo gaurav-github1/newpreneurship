953be3bd6c1c6c09f024ba199d73d2afdda807fb
Gaurav Verma <gv29403@gmail.com>
2025-07-20 15:01:11 +0530

    fix the easymde

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,58 @@
+import { UserIcon } from "lucide-react";
+import { defineField, defineType } from "sanity";
+
+export const startup = defineType(
+    {
+        name: 'startup',
+        title: 'Startup',
+        type: 'document',
+        fields: [
+            defineField({
+                name: 'title',
+                title: 'Title',
+                type: 'string',
+            }),
+            defineField({
+                name: 'slug',
+                title: 'Slug',
+                type: 'slug',
+                options:{
+                    source: 'title',
+                }
+            }),
+            defineField({
+                name: 'author',
+                title: 'Author',
+                type: 'reference',
+                to: [{type: 'author'}],
+            }),
+            defineField({
+                name: 'views',
+                title: 'number of views',
+                type: 'number',
+            }),
+            defineField({
+                name: 'description',
+                title: 'Description',
+                type: 'text',
+            }),
+            defineField({
+                name: 'category',
+                title: 'Category',
+                type: 'string',
+                validation: (Rule) => Rule.required().min(3).max(50).error('Category must be between 3 and 50 characters long'),
+            }),
+            defineField({
+                name: 'image',
+                title: 'Image',
+                type: 'url',
+                validation: (Rule)=> Rule.required()
+            }),
+            defineField({
+                name: 'pitch',
+                title: 'Pitch',
+                type: 'markdown',
+            }),
+        ],
+    }
+)
